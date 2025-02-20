335e87c214caf4377a99ed850f23a0f548620f78
jijoy98 <90660002+jijoy98@users.noreply.github.com>
2025-02-20 08:36:42 -0500

    Add files via upload

--- name_formatter.py
+++ name_formatter.py
@@ -5,6 +5,9 @@ def get_formatted_name(first_name, middle_name, last_name):
     else:
         full_name = f"{first_name} {last_name}"
     return full_name.title()
+firt_name = input("John:")
+middle_name = input("K:")
+last_name = input ("Doe:")
 musician = get_formatted_name('jimi', '', 'hendrix')
 print(musician)
 musician = get_formatted_name('john', 'lee', 'hooker')  
